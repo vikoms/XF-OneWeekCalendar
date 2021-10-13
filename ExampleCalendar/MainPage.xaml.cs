79dd2a17ffc31a44d5ca3009024e33287072b63b
Viko Muhammad Saputra <viko.s@bts.id>
2021-10-13 10:46:04 +0700

    init commit

---
+++ ExampleCalendar/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ExampleCalendar
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void ChangeSelectedDate_Clicked(System.Object sender, System.EventArgs e)
+        {
+        }
+
+        void CalendarWeekControl_SelectedDateChanged(System.Object sender, ExampleCalendar.Models.SelectedDateChangedEventArgs e)
+        {
+        }
+    }
+}
